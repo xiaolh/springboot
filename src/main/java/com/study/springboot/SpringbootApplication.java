c5fbcaea35d96956e98f996e83856bd495158622
xiaolh <acexlh@live.com>
2020-04-28 09:10:39 +0800

    # 完善代码

--- src/main/java/com/study/springboot/SpringbootApplication.java
+++ src/main/java/com/study/springboot/SpringbootApplication.java
@@ -1,7 +1,10 @@
 package com.study.springboot;
 
 import com.study.springboot.basic.utils.LoginInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,11 +13,12 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistration
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@Slf4j
 @SpringBootApplication
 @EnableScheduling
 @EnableCaching
 @MapperScan(basePackages = {"com.study.springboot.*.mapper"})
-public class SpringbootApplication extends WebMvcConfigurerAdapter {
+public class SpringbootApplication extends WebMvcConfigurerAdapter implements ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
@@ -32,7 +36,13 @@ public class SpringbootApplication extends WebMvcConfigurerAdapter {
         registration.excludePathPatterns("/**/*login*");
         registration.excludePathPatterns("/**/*regist*");
         registration.excludePathPatterns("/js/**");
+        registration.excludePathPatterns("/img/**");
         super.addInterceptors(registry);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        log.info("============================================================= STARTED =============================================================");
     }
 
 }
